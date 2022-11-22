df09b422868a869ed79e8960f667067e92aebe25
rezcode <rezharians@gmail.com>
2022-11-22 19:22:05 +0800

    add gap at banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Ilustration1 from "../assets/images/banner.png";
 const Home = () => {
   return (
     <>
-      <div className="px-4 items-center mx-auto lg:max-w-7xl md:px-8 grid md:grid-cols-10 pt-6">
+      <div className="px-4 items-center mx-auto lg:max-w-7xl gap-6 md:px-8 grid md:grid-cols-10 pt-6">
         <div className="col-span-5 md:order-last">
           <img src={Ilustration1} alt="banner" />
         </div>
@@ -13,9 +13,10 @@ const Home = () => {
             Want anything to be easy <br />
             <span className="font-bold">with LaslesVPN.</span>
           </h1>
-          <div className="font-medium text-xs pb-12">
-            Provide a network for all your needs with ease and fun using
-            LaslesVPN discover interesting features from us.
+          <div className="font-medium text-base pb-12">
+            Provide a network for all your needs with ease and fun using{" "}
+            <span className="font-bold">LaslesVPN</span> discover interesting
+            features from us.
           </div>
           <button className="py-4 px-16 bg-red-500 drop-shadow-3xl rounded-md text-white">
             Get Started
